5010f920272c52a0a7f60cc81ac8301b41f48246
sym19940907 <32972357+sym19940907@users.noreply.github.com>
2017-12-10 10:31:48 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,86 +1,50 @@
-from flask import Flask, request, render_template, jsonify
-from subprocess import *
-import random
+from flask import *
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
 application = Flask(__name__)
 
-AWS_ACCESS_KEY = ''
-AWS_SECRET_KEY = ''
-region = 'us-east-1'
+# AWS_ACCESS_KEY = 'j'
+# AWS_SECRET_KEY = 'k'
+# region = 'us-east-1'
+#
+# awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, 'es')
+#
+# host = 'search-test-qaqcu3tpmtqedbutv744acsjm4.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
+#
+# es = Elasticsearch(
+#     hosts=[{'host': host, 'port': 443}],
+#     http_auth=awsauth,
+#     use_ssl=True,
+#     verify_certs=True,
+#     connection_class=RequestsHttpConnection
+# )
 
-awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, 'es')
 
-host = 'search-test-qaqcu3tpmtqedbutv744acsjm4.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
-
-es = Elasticsearch(
-    hosts=[{'host': host, 'port': 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=RequestsHttpConnection
-)
+@application.route("/", methods=['GET', 'POST', 'PUT'])
+def initial_page():
+    print request
 
 
-@application.route("/", methods=['GET', 'POST'])
-def main_page():
-    if request.form['type'] == 'login':  # an existing user is going to log in
-        if not es.exists(index="users", doc_type="default", id=request.form['userId']):
-            return jsonify({'status': 'failed', 'message': 'user does not exist'})
-        else:
-            user_info = es.get(index="users", doc_type="default", id=request.form['userId'])
-            if user_info['_source']['password'] == request.form['info']['password']:
-                pass
-                return_json = user_info['_source']
-                return_json.pop('password')
-                return_json['status'] = 'success'
+    if request.method == 'POST':
+        if request.form['message'] == 'login':
+            print request.form['message']
+            print 'jjj'
+            return redirect('login')
+    return render_template('weshop.html')
 
-                # do something to return the event that most likely to be attended by the user
 
-                return jsonify(return_json)
-            else:
-                return jsonify({'status': 'failed', 'message': 'incorrect password'})
+@application.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
-    elif request.form['type'] == 'signup':  # a new user is going to sign up a new account
-        if es.exists(index="users", doc_type="default", id=request.form['userId']):
-            return jsonify({'status': 'failed', 'message': 'user already exists'})
-        else:
-            user_information = request.form['info']
-            user_information['friends'] = []
-            user_information['invited_events'] = []
-            user_information['attending_events'] = []
-            user_information['pending_friend_requests'] = []
-            es.index(index="users", doc_type="default", id=request.form['userId'], body=user_information)
-            return jsonify({'status': 'success', 'message': 'signup succeeded'})
 
-    elif request.form['type'] == 'create_event':
-        pass
-    elif request.form['type'] == 'attend_event':  # a user agrees to attend a event
-        pass
-    elif request.form['type'] == 'add_friend':  # user1 sends friend request to user2
-        if not es.exists(index="users", doc_type="default", id=request.form['info']['user2']):
-            return jsonify({"status": "failed", "message": "user2 does not exist"})
-        else:
-            user_info = es.get(index="users", doc_type="default", id=request.form['info']['user2'])['_source']
-            user_info['pending_friend_requests'].append(request.form['info']['user1'])
-            es.index(index="users", doc_type="default", id=request.form['info']['user2'], body=user_info)
-            return jsonify({"status": "succeed", "message": "friend request sent"})
-    elif request.form['type'] == 'add_friend_agree':  # user2 agrees to be friend with user1
-        user_info = es.get(index="users", doc_type="default", id=request.form['info']['user2'])['_source']
-        if request.form['info']['user1'] in user_info['pending_friend_requests']:
-            user_info['pending_friend_requests'].remove(request.form['info']['user1'])
-        user_info['friends'].append(request.form['info']['user1'])
-        es.index(index="users", doc_type="default", id=request.form['info']['user2'], body=user_info)
-        return({'status': 'succ'})
+@application.route("/signup", methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
 
-    elif request.form['type'] == 'add_friend_decline':  # user2 declines to be friend with user1
-        user_info = es.get(index="users", doc_type="default", id=request.form['info']['user2'])['_source']
-        if request.form['info']['user1'] in user_info['pending_friend_requests']:
-            user_info['pending_friend_requests'].remove(request.form['info']['user1'])
-        es.index(index="users", doc_type="default", id=request.form['info']['user2'], body=user_info)
-
-    return render_template('main_page.html')
 
 if __name__ == '__main__':
     application.run()
+
+
