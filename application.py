6a59f5efc3d7f20f27a20b96c74b27ecccab149b
sym19940907 <32972357+sym19940907@users.noreply.github.com>
2017-12-10 19:28:33 -0500

    Update application.py

--- application.py
+++ application.py
@@ -4,28 +4,26 @@ from requests_aws4auth import AWS4Auth
 
 application = Flask(__name__)
 
-# AWS_ACCESS_KEY = 'j'
-# AWS_SECRET_KEY = 'k'
-# region = 'us-east-1'
-#
-# awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, 'es')
-#
-# host = 'search-test-qaqcu3tpmtqedbutv744acsjm4.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
-#
-# es = Elasticsearch(
-#     hosts=[{'host': host, 'port': 443}],
-#     http_auth=awsauth,
-#     use_ssl=True,
-#     verify_certs=True,
-#     connection_class=RequestsHttpConnection
-# )
+AWS_ACCESS_KEY = 'j'
+AWS_SECRET_KEY = 'k'
+region = 'us-east-1'
+
+awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, 'es')
+
+host = 'search-test-qaqcu3tpmtqedbutv744acsjm4.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
 
 
 @application.route("/", methods=['GET', 'POST', 'PUT'])
 def initial_page():
-    print request
-
-
+    print request.form
     if request.method == 'POST':
         if request.form['message'] == 'login':
             print request.form['message']
@@ -36,15 +34,40 @@ def initial_page():
 
 @application.route("/login", methods=['GET', 'POST'])
 def login():
+    print request.form
+    if request.method == 'POST':
+        if not es.exists(index="users", doc_type="default", id=request.form['userId']):
+            return jsonify({'status': 'failed', 'message': 'user does not exist'})
+        else:
+            user_info = es.get(index="users", doc_type="default", id=request.form['userId'])
+            if user_info['_source']['password'] == request.form['info']['password']:
+                return_json = user_info['_source']
+                return_json.pop('password')
+                return_json['status'] = 'success'
+
+                return jsonify(return_json)
+            else:
+                return jsonify({'status': 'failed', 'message': 'incorrect password'})
     return render_template('login.html')
 
 
 @application.route("/signup", methods=['GET', 'POST'])
 def signup():
+    print request.form
+    if request.method == 'POST':
+        if es.exists(index="users", doc_type="default", id=request.form['userId']):
+            return jsonify({'status': 'failed', 'message': 'user already exists'})
+        else:
+            user_information = request.form['info']
+            user_information['friends'] = []
+            user_information['invited_events'] = []
+            user_information['attending_events'] = []
+            user_information['pending_friend_requests'] = []
+            es.index(index="users", doc_type="default", id=request.form['userId'], body=user_information)
+            return jsonify({'status': 'success', 'message': 'signup succeeded'})
     return render_template('signup.html')
 
 
 if __name__ == '__main__':
     application.run()
-
 
